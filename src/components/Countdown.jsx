084b3893498f7e6731cca96d639fbfce8a034754
William M. Tsikata <tsikatawill@gmail.com>
2022-02-01 04:09:46 +0000

    installed react-icons

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -6,30 +6,30 @@ const Countdown = () => {
   const [mins, setMins] = React.useState(9);
   const [secs, setSecs] = React.useState(12);
 
-  // React.useEffect(() => {
-  //   const eta = new Date("December 25, 2022 00:00:00").getTime();
-  //   const today = new Date().getTime();
-  //   const gap = eta - today;
+  React.useEffect(() => {
+    const eta = new Date("December 25, 2022 00:00:00").getTime();
+    const today = new Date().getTime();
+    const gap = eta - today;
 
-  //   // convert to milliseconds
-  //   const second = 1000;
-  //   const min = second * 60;
-  //   const hr = min * 60;
-  //   const day = hr * 24;
+    // convert to milliseconds
+    const second = 1000;
+    const min = second * 60;
+    const hr = min * 60;
+    const day = hr * 24;
 
-  //   let dayss = Math.floor(gap / day);
-  //   let hourss = Math.floor((gap % day) / hr);
-  //   let minss = Math.floor((gap % hr) / min);
-  //   let secss = Math.floor((gap % min) / second);
+    let dayss = Math.floor(gap / day);
+    let hourss = Math.floor((gap % day) / hr);
+    let minss = Math.floor((gap % hr) / min);
+    let secss = Math.floor((gap % min) / second);
 
-  //   setInterval(() => {
-  //     setDays(dayss);
-  //     setHours(hourss);
-  //     setMins(minss);
-  //     setSecs(secss);
-  //   }, 1000);
-  //   console.log("Render");
-  // });
+    setInterval(() => {
+      setDays(dayss);
+      setHours(hourss);
+      setMins(minss);
+      setSecs(secss);
+    }, 1000);
+    console.log("Render");
+  });
   return (
     <div className="countdown my-4">
       <div className="time-display">
