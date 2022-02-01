ff6a4cb0bad2320a76fd75419b6998809695e6c9
William M. Tsikata <tsikatawill@gmail.com>
2022-02-01 04:13:55 +0000

    added an alt attribute to an image

--- src/components/ProductsOne.jsx
+++ src/components/ProductsOne.jsx
@@ -5,7 +5,7 @@ import Stats from "../assets/stats.svg";
 const ProductsOne = () => {
   return (
     <Products
-      image={<img src={Stats} className="img-fluid" />}
+      image={<img src={Stats} className="img-fluid" alt="some" />}
       title="Our Products Are"
       primarytext="Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque fuga, a porro natus cum tenetur debitis provident, eos voluptatibus optio dolorem. Vel architecto veritatis molestias hic illum voluptate repellat. Beatae!"
       secondarytext="Itaque fuga, a porro natus cum tenetur debitis provident, eos voluptatibus optio dolorem. Vel architecto veritatis molestias hic illum voluptate repellat. Beatae!"
