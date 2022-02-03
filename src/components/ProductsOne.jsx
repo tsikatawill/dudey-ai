2d3541ed914e09eb72b4965a44d969a63567d9cd
William M. Tsikata <tsikatawill@gmail.com>
2022-02-03 13:44:12 +0000

    changed productsOne image

--- src/components/ProductsOne.jsx
+++ src/components/ProductsOne.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Products from "./Products";
-import Stats from "../assets/stats.svg";
+import ProdOne from "../assets/productsOne.jpeg";
 
 const ProductsOne = () => {
   return (
     <Products
-      image={<img src={Stats} className="img-fluid" alt="some" />}
+      image={<img src={ProdOne} className="img-fluid" alt="some" />}
       title="Our Products Are"
       primarytext="Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque fuga, a porro natus cum tenetur debitis provident, eos voluptatibus optio dolorem. Vel architecto veritatis molestias hic illum voluptate repellat. Beatae!"
       secondarytext="Itaque fuga, a porro natus cum tenetur debitis provident, eos voluptatibus optio dolorem. Vel architecto veritatis molestias hic illum voluptate repellat. Beatae!"
